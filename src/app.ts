1c6867a4a30bedd095abb3e8be61ffe9fde45809
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-10-18 21:53:14 -0300

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma =  new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'Clelton José Mielke',
+        email: 'cleltonjm_95@hotmail.com'
+    }
+})
