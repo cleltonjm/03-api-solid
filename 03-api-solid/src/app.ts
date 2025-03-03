dbfb36588771140ac539a8c58627821df8c559ac
Clelton <cleltonjm_95@hotmail.com>
2025-03-02 23:03:24 -0300

    Instalando Prisma ORM

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,3 +1,15 @@
 import fastify  from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'Clelton Jose Mielke',
+        email: 'cleltonjm_95@hotmail.com',
+    },
+})
+
+// ORM - Object Relational Mapper
