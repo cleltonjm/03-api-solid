125dd4dcac0d304ec8c9d4cc3983c0459f1f561d
Clelton <cleltonjm_95@hotmail.com>
2025-03-03 15:44:31 -0300

    Casos de usos e design patters

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,15 +1,22 @@
 import fastify  from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user.create({
-    data: {
-        name: 'Clelton Jose Mielke',
-        email: 'cleltonjm_95@hotmail.com',
-    },
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({ message: 'Validantion error.', issues: error.format() })
+    }
+
+    if (env.NODE_ENV !== 'production') {
+        console.error(error)
+    } else {
+        // TODO here we should log to an external tool like Datalog
+    }
+
+    return reply.status(500).send({ message: 'Internal server error.'})
 })
-
-// ORM - Object Relational Mapper
