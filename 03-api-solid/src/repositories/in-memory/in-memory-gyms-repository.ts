c25f6b1ae0634a3fad9b819298f552cefbbd4637
Clelton <cleltonjm_95@hotmail.com>
2025-03-12 20:58:02 -0300

    Create prisma repository

--- 03-api-solid/src/repositories/in-memory/in-memory-gyms-repository.ts
+++ 03-api-solid/src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,5 +1,5 @@
 import { Gym, Prisma } from "@prisma/client";
-import { FindManyNearbyParams, GymsRepository } from "../gym-repository";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
 import { randomUUID } from "node:crypto";
 import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
